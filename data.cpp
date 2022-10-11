198f575ec612c718c0294861fd19de858337bdd3
yuvraj <demonsroar2172@gmail.com>
2022-10-12 04:50:58 +0530

    Storing Data in Cpp

---
+++ data.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a=100;
+    char c =a;
+    cout<<c<<endl;
+
+    int b=0160;
+    char d=b;
+    cout<<d<<endl;;
+
+    float f=1.2;
+    a=f;
+    cout<<a<<endl;
+
+    f=1.9;
+    a=f;
+    cout<<a<<endl;
+
+    c='a';
+    int i=10;
+    int output=i+c;
+    cout<<output<<endl;
+
+    c=i+c;
+    cout<<c<<endl;
+}
