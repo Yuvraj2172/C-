e3c34b610707f44482c5d8ee4d6e034a5f8fc28b
yuvraj <demonsroar2172@gmail.com>
2022-10-12 20:53:35 +0530

    while Loop

---
+++ print1toN.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the Number"<<endl;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        cout<<i<<endl;
+        i+=1;
+    }
+}
