b0134a6024fc75bf937e5128511d9c02ec556a38
yuvraj <demonsroar2172@gmail.com>
2022-10-16 02:42:13 +0530

    nth fibonacci number

---
+++ Lec6/fibonaccinumber.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a=0,b=1;
+    for(int i=2;i<=n;i++){
+        int temp=a;
+        a=b;
+        b=a+temp;
+    }
+    cout<<b;
+}
