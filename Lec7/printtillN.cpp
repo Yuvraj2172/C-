0c86520ffc270e250b3df1452c1917ce3d5ff8ef
yuvraj <demonsroar2172@gmail.com>
2022-10-19 23:00:27 +0530

    Print till N

---
+++ Lec7/printtillN.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printtillN(int n){
+    for (size_t i = 1; i <= n; i++)
+    {
+        cout<<i<<" ";
+    }
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    printtillN(n);
+}
