d03ee0b691db5417e8770b04be68be5c3e51b41d
Yuvraj Soni <demonsroar2172@gmail.com>
2022-10-24 19:39:43 +0530

    Max Element in Array

--- Lec8/largestelement.cpp
+++ Lec8/largestelement.cpp
@@ -3,15 +3,21 @@ using namespace std;
 int main(){
     int n ;
     cin>>n;
+    if(n>0){
     int arr[100];
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
     int max = arr[0];
-    for(int i=0;i<n;i++){
+    for(int i=1;i<n;i++){
         if(arr[i]>max){
             max=arr[i];
         }
     }
     cout<<"Largest Element is : "<<max;
+    }
+    else {
+        cout<<"Max : "<<INT_MIN;
+    }
+
 }
