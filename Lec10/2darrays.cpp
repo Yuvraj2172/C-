ff266351d961e5b9b07f8193acddd90a745ea304
Yuvraj Soni <demonsroar2172@gmail.com>
2022-11-08 16:33:17 +0530

    Array I/O

--- Lec10/2darrays.cpp
+++ Lec10/2darrays.cpp
@@ -4,14 +4,25 @@ int main(){
     int a[100][100];
     int m,n;
     cin>>m>>n;
+    //Taking input
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             cin>>a[i][j];
         }
     }
+
+    //Printing array row-wise
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+     cout<<endl;
+    //Printing array column-wise
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<a[j][i]<<" ";
         }
         cout<<endl;
     }
