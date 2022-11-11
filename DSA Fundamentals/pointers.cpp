0cecdce565fb93d4efcc6c5a04a5af41337dae30
Yuvraj Soni <demonsroar2172@gmail.com>
2022-11-11 08:51:16 +0530

    Pointers

--- DSA Fundamentals/pointers.cpp
+++ DSA Fundamentals/pointers.cpp
@@ -1,5 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+    //Pointers are variables which stores the address of another variables.
+    // int *p = &i; // p is a pointer to an integer.
+    // char *ch = &c // ch is a pointer to a character.
+   int i=10;
+   cout<<&i<<endl;
+
+   float f = 10.2;
+   float *pf = &f;
+    double d = 1222.32;
+    double *pd = &d;
     
+//    int *p = &i;
+//    *p=*p+6;
+//    cout<<*p<<" "<<i;
 }
