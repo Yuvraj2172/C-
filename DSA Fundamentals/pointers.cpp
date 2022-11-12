07a83afd69851faeb752c9444e6bd9892ee93430
Yuvraj Soni <demonsroar2172@gmail.com>
2022-11-12 21:29:33 +0530

    Init

--- DSA Fundamentals/pointers.cpp
+++ DSA Fundamentals/pointers.cpp
@@ -5,14 +5,27 @@ int main(){
     // int *p = &i; // p is a pointer to an integer.
     // char *ch = &c // ch is a pointer to a character.
    int i=10;
-   cout<<&i<<endl;
-
-   float f = 10.2;
-   float *pf = &f;
-    double d = 1222.32;
-    double *pd = &d;
+//    cout<<&i<<endl;
+    int *p=&i;
+//    float f = 10.2;
+//    float *pf = &f;
+//     double d = 1222.32;
+//     double *pd = &d;
+    // cout<<sizeof(*p);
     
 //    int *p = &i;
 //    *p=*p+6;
 //    cout<<*p<<" "<<i;
+
+// cout<< i++<<endl;
+// cout<<i<<endl;
+// cout<<*p;
+    int a = *p;
+    a++;
+    // cout<<a<<endl;
+    // cout<<*p<<endl;
+
+    int *q =0;
+    cout<<"EXEC";
+    cout<<*q<<endl;
 }
