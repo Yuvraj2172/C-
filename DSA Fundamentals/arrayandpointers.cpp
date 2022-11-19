a6eec5d55966e922bb2cc4dff90c25027bb1f2cf
Yuvraj Soni <demonsroar2172@gmail.com>
2022-11-19 17:47:49 +0530

    Array and Pointers

--- DSA Fundamentals/arrayandpointers.cpp
+++ DSA Fundamentals/arrayandpointers.cpp
@@ -1,35 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
 
-    //You cannot change any value in symbol table. 
-    //Array cannot be reassigned ... 
-    // Not Valid...    a=a+3
+    // You cannot change any value in symbol table.
+    // Array cannot be reassigned ...
+    //  Not Valid...    a=a+3
 
     int a[10];
-    cout<<a<<endl;
-    cout<<&a[0]<<endl;
+    cout << a << endl;
+    cout << &a[0] << endl;
 
-    a[0]=5;
-    a[1]=10;
-    cout<<*a<<endl;
-    cout<<*(a+1)<<endl;
+    a[0] = 5;
+    a[1] = 10;
+    cout << *a << endl;
+    cout << *(a + 1) << endl;
 
-    int *p =&a[0];
-    cout<<a<<endl;
-    cout<<p<<endl;
-    cout<<&p<<endl;
-    cout<<&a<<endl;
+    int *p = &a[0];
+    cout << a << endl;
+    cout << p << endl;
+    cout << &p << endl;
+    cout << &a << endl;
 
-    cout<<sizeof(p)<<endl;
-    cout<<sizeof(a)<<endl;
+    cout << sizeof(p) << endl;
+    cout << sizeof(a) << endl;
 
-    p = p +1; //Allowed
+    p = p + 1; // Allowed
     // a=a+1; Not Allowed . Error
-   // a++;  Not Allowed . Error
+    // a++;  Not Allowed . Error
 
-    p=a+1;
-    cout<<*p;
-    
+    p = a + 1;
+    cout << *p<<endl;
+
+    int ar[6] = {1, 2, 3};
+    int *b = ar;
+    cout <<"This "<< b[2]<<endl;
+    cout<<sizeof(b)<<endl;
+
 
 }
