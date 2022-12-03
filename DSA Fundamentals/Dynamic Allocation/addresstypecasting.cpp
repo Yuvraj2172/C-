13f2ee8795802b0f03135c00c31e97a4f285315f
Yuvraj Soni <demonsroar2172@gmail.com>
2022-12-03 15:52:08 +0530

    Dynamic Allocation

---
+++ DSA Fundamentals/Dynamic Allocation/addresstypecasting.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int i=67;
+    // char c=i;
+    // cout<<c<<endl;
+
+    // int *p = &i;
+    // char *pc = (char*)p;
+    // cout<<p<<endl;
+    // cout<<(pc)<<endl;
+    // cout<<*(pc+1)<<endl;
+    // cout<<*(pc+2)<<endl;
+    // cout<<*(pc+3)<<endl;
+
+    char c = 'A';
+    char *ch= &c;
+    int *p=(int*)ch;
+    cout<<*ch<<endl;
+    cout<<*p<<endl;
+    
+
+}
