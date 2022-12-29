8040c3705e100ab70c087b717a9b4256305e3822
Yuvraj Soni <demonsroar2172@gmail.com>
2022-12-29 19:39:50 +0530

    OOPS lec-2

--- DSA Fundamentals/OOPS-1/student.cpp
+++ DSA Fundamentals/OOPS-1/student.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Student{
+    public :
     int roll;
     int age;
 };
+
+// int main(){
+//     Student s1;  
+//     cout<<s1.roll<<" "; 
+//     Student s2;
+//     cout<<s2.roll<<" ";
+//     Student *s3 = new Student;
+//     cout<<s3->roll<<" ";
+// }
---
+++ DSA Fundamentals/OOPS-1/studentuse.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+#include "Student.cpp"
+int main(){
+    //Creates objects statically
+    Student s1;
+    Student s2;
+    s1.roll = 12;
+    s1.age = 20;
+    s2.age = 21;
+    s2.roll = 45;
+    cout<<s1.roll<<" "<<s1.age<<"\n";
+    cout<<s2.roll<<" "<<s2.age<<"\n";
+    //Creates objects dynamically.
+    Student *s4 = new Student;
+    (*s4).age =15;
+    (*s4).roll = 19;
+
+    //This will also work...Just a way of dereferencing... 
+    // s4->roll = 15;
+    // s4->age = 19;
+
+    cout<<s4->roll<<" "<<s4->age<<"\n";
+    // Student s5 = new Student; // Wrong Sytax
+    Student s6;
+    // new Student s7;  // Wrong Syntax
+}
