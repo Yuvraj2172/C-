49e71ee7c31f863c109baac513e174538f6a755a
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-01 03:06:05 +0530

    Added minor changes and a divide functionality

--- DSA Fundamentals/OOPS-1/fraction.cpp
+++ DSA Fundamentals/OOPS-1/fraction.cpp
@@ -19,7 +19,7 @@ public:
     void Simplify(){
         int gcd =1;
         int j = min(this->numerator, this->denominator);
-        for(int i=1; i<j;i++){
+        for(int i=1; i<=j;i++){
             if(this->numerator%i==0 && this->denominator%i==0){
                 gcd = i;
             }
@@ -27,7 +27,7 @@ public:
         this->numerator = this->numerator/gcd;
         this->denominator = this->denominator/gcd;
     }
-    void add(Fraction f2){
+    void add(Fraction const &f2){
         int lcm = denominator*f2.denominator;
         int x = lcm/denominator;
         int y = lcm/f2.denominator;
@@ -37,13 +37,29 @@ public:
         denominator = lcm;
         Simplify();
     }
+    void multiply(Fraction const &f2){
+        numerator = numerator * f2.numerator;
+        denominator = denominator * f2.denominator;
+        Simplify();
+    }
+     void divide(Fraction const &f2){
+        numerator = numerator * f2.denominator;
+        denominator = denominator * f2.numerator;
+        Simplify();
+    }
 };
 int main(){
-    Fraction f1(10,2);
+    Fraction f1(10,4);
     // f1.Print();
     Fraction f2(15,4);
-    f1.add(f2);
+    // f1.add(f2);
     f1.Print();
     f2.Print();
+    cout<<"Multiplying: ";
+    f1.multiply(f2);
+    f1.Print();
+    cout<<"Dividing: ";
+    f1.divide(f2);
+    f1.Print();
 
 }
