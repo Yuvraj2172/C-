9afce0944f349a10b3b1ff3479ab8f4ffe2b950f
Yuvraj Soni <demonsroar2172@gmail.com>
2022-12-31 23:43:46 +0530

    Playing with constructors

---
+++ DSA Fundamentals/OOPS-1/playwithconstructors.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include "Student.cpp"
+int main(){
+    Student s1; //Constructor 1 will be called
+    Student s2(101); // Constructor 2 will be callled
+    Student s3(20,102); //Constructor 3 will be called
+    Student s4(s3); //Copy constructor will be called
+    s1 = s2; // Copy assignment operator... No constructor calling 
+    Student s5 = s4;
+
+
+}
