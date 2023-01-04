1bfec37e71ba9ac68a4f993abcc34bba87299ad8
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-05 00:00:56 +0530

    Printing Linked List

--- DSA Fundamentals/Linked List - 1/linkedlistuse.cpp
+++ DSA Fundamentals/Linked List - 1/linkedlistuse.cpp
@@ -1,12 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 #include "ll.cpp"
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+}
 int main()
 {
-    Node n1(10);
-    Node n2(20);
-    n1.next = &n2;
+    // Statically
+
+    Node n1(1);
     Node *head = &n1;
-    cout << head->data << endl;
-    cout << head->next->data << endl;
+    Node n2(2);
+    n1.next = &n2;
+    Node n3(3);
+    n2.next = &n3;
+    Node n4(4);
+    n3.next = &n4;
+    Node n5(5);
+    n4.next = &n5;
+    print(head);
+
+    // Dynamically
+    // Node *n3= new Node(10);
+    // Node *head2  = n3;
+    // Node *n4 = new Node(20);
+    // n3->next = n4;
 }
