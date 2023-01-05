824d4f1e8e9564419844e567b8ef15edd01a9fee
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-05 22:58:26 +0530

    Taking input from user in a linked list

--- DSA Fundamentals/Linked List - 1/linkedlistuse.cpp
+++ DSA Fundamentals/Linked List - 1/linkedlistuse.cpp
@@ -9,26 +9,51 @@ void print(Node *head)
         cout << temp->data << " -> ";
         temp = temp->next;
     }
+    cout<<"END\n";
+}
+Node* takeInput(){
+   int data;
+   cout<<"Enter the elements one by one : \n";
+   cin>>data;
+   Node *head = NULL;
+   while(data!=-1){
+    Node *newnode = new Node(data);
+    if(head==NULL){
+        head = newnode;
+    }
+    else {
+        Node *temp = head;
+        while(temp->next !=NULL){
+            temp = temp ->next;
+        }
+        temp->next = newnode;
+    }
+    cin>>data;
+   }
+   return head;
 }
 int main()
 {
     // Statically
 
-    Node n1(1);
-    Node *head = &n1;
-    Node n2(2);
-    n1.next = &n2;
-    Node n3(3);
-    n2.next = &n3;
-    Node n4(4);
-    n3.next = &n4;
-    Node n5(5);
-    n4.next = &n5;
-    print(head);
+    // Node n1(1);
+    // Node *head = &n1;
+    // Node n2(2);
+    // n1.next = &n2;
+    // Node n3(3);
+    // n2.next = &n3;
+    // Node n4(4);
+    // n3.next = &n4;
+    // Node n5(5);
+    // n4.next = &n5;
+    // print(head);
 
     // Dynamically
     // Node *n3= new Node(10);
     // Node *head2  = n3;
     // Node *n4 = new Node(20);
     // n3->next = n4;
+
+    Node *head =takeInput();
+    print(head);    
 }
