76d52839272bb186e7a404fcaadd3d40d7c8023f
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-06 01:51:26 +0530

    Deleting a node

---
+++ DSA Fundamentals/Linked List - 1/lldeletingnode.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+// #include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+Node *deleteNode(Node *head, int pos)
+{
+	if(pos==0){
+		head = head->next;
+		return head;
+	}
+	// int idx= 0;
+	Node *temp=head;
+	for(int i=0;i<pos -1 && (temp !=NULL);i++){
+		temp = temp->next;
+	}
+	if(temp->next!=NULL){
+		temp ->next = temp->next ->next;
+		return head;
+	}
+	return head;
+	
+}
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		head = deleteNode(head, pos);
+		print(head);
+	}
+
+	return 0;
+}
