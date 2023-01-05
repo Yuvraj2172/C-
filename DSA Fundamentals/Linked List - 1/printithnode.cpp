c151f30fa9ddec8db96af262f5dbc27c731ad765
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-05 23:44:44 +0530

    Print ith Node of a Linked List

---
+++ DSA Fundamentals/Linked List - 1/printithnode.cpp
@@ -0,0 +1,67 @@
+// For a given a singly linked list of integers and a position 'i', print the node data at the 'i-th' position.
+
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+// #include "solution.h"
+void printIthNode(Node *head, int i)
+{
+    int idx=0;
+	while(head!=NULL){
+		if(i==idx){
+			cout<<head->data;
+			return;
+		}
+		idx++;
+		head = head ->next;
+	}
+}
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		int pos;
+		cin >> pos;
+		printIthNode(head, pos);
+		cout << endl;
+	}
+	return 0;
+}
