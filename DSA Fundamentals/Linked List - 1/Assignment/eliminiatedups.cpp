710eb98d1d90a45fd6ad16d221fcfae44247fd78
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-08 17:34:49 +0530

    Delete Duplicates

---
+++ DSA Fundamentals/Linked List - 1/Assignment/eliminiatedups.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+// #include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+Node *removeDuplicates(Node *head)
+{
+  Node *temp = head;
+  if (head == NULL || head->next == NULL) {
+    return head;
+  }
+  while (temp != NULL && temp->next != NULL) {
+    if (temp->data == temp->next->data) {
+		Node* t2 = temp->next;
+      temp->next = temp->next->next;
+ 
+    } else {
+      temp = temp->next;
+    }
+  }
+  return head;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		head = removeDuplicates(head);
+		print(head);
+	}
+	return 0;
+}
