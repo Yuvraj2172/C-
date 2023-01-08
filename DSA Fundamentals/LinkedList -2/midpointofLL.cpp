3ea2b0e4d91771f714e5cbc60c5aa6c0e4dcf651
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-08 20:59:44 +0530

    Mid Point of a LinkedLlist

---
+++ DSA Fundamentals/LinkedList -2/midpointofLL.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+// #include "solution.h"
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}/****************************************************************
+ 
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+Node *midPoint(Node *head)
+{
+	if(head==NULL || head->next==NULL)return head;
+    Node* slow = head;
+	Node* fast = head->next;
+	while(fast!=NULL && fast->next !=NULL){
+		slow = slow ->next;
+		fast = fast->next ->next;
+	}
+	return slow;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		Node *mid = midPoint(head);
+		if (mid != NULL)
+		{
+			cout << mid->data;
+		}
+		cout << endl;
+	}
+	return 0;
+}
