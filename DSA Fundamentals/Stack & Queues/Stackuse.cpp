1205d581d07dc59b75255f585177068fd975a1b0
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-16 16:39:33 +0530

    Inbuilt Stack

--- DSA Fundamentals/Stack & Queues/Stackuse.cpp
+++ DSA Fundamentals/Stack & Queues/Stackuse.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
-#include "StackUsingArray.cpp"
+#include "StackUsingArray.h"
+#include <stack>
 int main(){
-    StackUsingArray st;
+  /*  StackUsingArray st;
     st.push(10);
     st.push(20);
     st.push(30);
@@ -16,5 +17,18 @@ int main(){
 
     cout<<st.size()<<endl;
     cout<<st.isEmpty();
-
+*/
+   stack<int> s;
+   s.push(10);
+   s.push(20);
+   s.push(30);
+   s.push(40);
+   s.push(50);
+   s.push(60);
+   cout<<s.size()<<"\n";
+   cout<<s.top()<<"\n";
+   s.pop();
+   cout<<s.top()<<"\n";
+   cout<<s.size()<<"\n";
+   cout<<s.empty()<<"\n";
 }
