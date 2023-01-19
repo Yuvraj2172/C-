af803588ecc7ecefa245060ad0ee671b5ba464dc
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-19 11:15:13 +0530

    Levelwise input in Tree

--- DSA 2/Trees/Treeuse.cpp
+++ DSA 2/Trees/Treeuse.cpp
@@ -11,12 +11,12 @@ TreeNode<int>* takeInputLevelwise(){
     while(!pendingNodes.empty()){
         TreeNode<int>* front = pendingNodes.front();
         pendingNodes.pop();
-        cout<<"Enter number of children of "<<front->data;
+        cout<<"Enter number of children of : "<<front->data;
         int numChild;
         cin>>numChild;
         for(int i=0;i<numChild;i++){
             int childData ;
-            cout<<"Enter "<<i<<" th child of "<<front->data<<endl;
+            cout<<"Enter "<<i<<" th child of : "<<front->data<<endl;
             cin>>childData;
             TreeNode<int>* child = new TreeNode<int>(childData);
             front-> children.push_back(child);
