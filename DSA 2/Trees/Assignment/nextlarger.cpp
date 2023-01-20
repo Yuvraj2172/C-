952a61c71a3e6a2f128169c61d874def95a7a6f2
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-20 19:40:26 +0530

    Second Largest Node in Tree

--- DSA 2/Trees/Assignment/nextlarger.cpp
+++ DSA 2/Trees/Assignment/nextlarger.cpp
@@ -1,21 +1,36 @@
 //  Next larger
 // Given a generic tree and an integer n. Find and return the node with next larger element in the tree i.e. find a node with value just greater than n.
 // Note: Return NULL if no node is present with the value greater than n.
-#include <bits/stdc++.h>
-void helper(TreeNode<int>* root, TreeNode<int>**  ans , int x,int* greatest){
-    if(root==NULL)return;
-    if((root->data > x) && root->data < *greatest){
-        *ans = root;
-        *greatest = root->data;
-        return;
+#include <queue>
+#include <climits>
+void helper(TreeNode<int>* root, TreeNode<int>** ans){
+  if (root == NULL) {
+    return;
+  }
+  queue<TreeNode<int>*> q;
+  TreeNode<int> *temp = root;
+  q.push(temp);
+  int largest  =INT_MIN;
+  int secondlargest =INT_MIN;
+  while (!q.empty()) {
+    TreeNode<int> *child = q.front();
+    q.pop();
+    if(child->data > largest){
+        secondlargest = largest;
+        largest = child->data;
     }
-    for(int i=0;i<root->children.size();i++){
-        helper(root->children[i],ans , x , greatest);
+    else if(child->data > secondlargest && child->data!=largest ){
+        secondlargest = child->data;
     }
+    for (int i = 0; i < child->children.size(); i++) {
+      q.push(child->children[i]);
+    }
+    }
+    *ans = new TreeNode<int>(secondlargest);
 }
-TreeNode<int>* getNextLargerElement(TreeNode<int>* root, int x) {
-    int greatest = INT_MAX;
-    TreeNode<int>* ans;
-    helper(root,&ans, x ,&greatest);
-    return ans;
+
+TreeNode<int>* getSecondLargestNode(TreeNode<int>* root) {
+   TreeNode<int> * ans;
+    helper(root, &ans);
+   return ans;
 }
