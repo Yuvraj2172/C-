674f4e4315dfac793acd38a75e9d43ee57866cd4
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-20 13:38:59 +0530

    Preorder Travesal

--- DSA 2/Trees/preordertraversal.cpp
+++ DSA 2/Trees/preordertraversal.cpp
@@ -118,6 +118,7 @@ TreeNode<int>* maxDataNode(TreeNode<int>* root) {
   return maxNode;
 }
 void preorder(TreeNode<int>* root){
+    if(root == NULL)return;
     cout<<root->data<<" ";
     for(int i=0;i<root->children.size();i++){
         preorder(root->children[i]);
