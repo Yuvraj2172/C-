a7e7d6dc4565d01ea85557b3f716f176b6f7c1e5
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-20 22:51:10 +0530

    BinaryTree

--- DSA 2/BinaryTree/BinaryTreeUse.cpp
+++ DSA 2/BinaryTree/BinaryTreeUse.cpp
@@ -1,10 +1,22 @@
 #include "BinaryTree.cpp"
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    BinartTreeNode<int>* root = new BinartTreeNode<int>(1);
-    BinartTreeNode<int>* node1 = new BinartTreeNode<int>(2);
-    BinartTreeNode<int>* node2 = new BinartTreeNode<int>(3);
+void printTree(BinartTreeNode<int> *root)
+{
+    if (root != NULL)
+    {
+        cout << root->data << "\n";
+        printTree(root->left);
+        printTree(root->right);
+    }
+    return;
+}
+int main()
+{
+    BinartTreeNode<int> *root = new BinartTreeNode<int>(1);
+    BinartTreeNode<int> *node1 = new BinartTreeNode<int>(2);
+    BinartTreeNode<int> *node2 = new BinartTreeNode<int>(3);
     root->left = node1;
     root->right = node2;
+    printTree(root);
 }
