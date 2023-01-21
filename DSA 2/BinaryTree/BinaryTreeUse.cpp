f61e35958bc6b090ac137dfe9571b35a05209f43
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-21 20:58:34 +0530

    PreOrder Travesal in Binary Tree

--- DSA 2/BinaryTree/BinaryTreeUse.cpp
+++ DSA 2/BinaryTree/BinaryTreeUse.cpp
@@ -108,6 +108,12 @@ void inOrder(BinaryTreeNode<int>* root ){
     cout<<root->data<<" ";
     inOrder(root->right);
 }
+void preOrder(BinaryTreeNode<int> *root) {
+	if(root == NULL)return;
+	cout<<root->data<<" ";
+	preOrder(root->left);
+	preOrder(root->right);
+}
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1 
 int main()
 {
