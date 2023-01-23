f8fb8e5244bd82782734d6d1ca55d5e14c87da6d
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-23 23:47:46 +0530

    Diameter of a Binary Tree

--- DSA 2/BinaryTree/BinaryTreeUse.cpp
+++ DSA 2/BinaryTree/BinaryTreeUse.cpp
@@ -120,6 +120,19 @@ void postOrder(BinaryTreeNode<int> *root) {
 	postOrder(root->right);
 	cout<<root->data<<" ";
 }
+int height(BinaryTreeNode<int>* root){
+    if(root == NULL)return 0;
+    return 1 + max(height(root->left) , height(root->right));
+}
+int diameter(BinaryTreeNode<int>* root){
+    if(root == NULL)return 0;
+    int option1 = height(root->left) + height(root->right);
+    int option2 = diameter(root->left);
+    int option3 = diameter(root->right);
+    // return max(option1 , option2 , option3);
+    int temp = max(option1 , option2);
+    return max(temp , option3);
+}
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1 
 int main()
 {
