112409fceeb8bc098508b42e841f2441ab9ce15f
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-23 23:52:32 +0530

    Diameter of BT in a better way

--- DSA 2/BinaryTree/BinaryTreeUse.cpp
+++ DSA 2/BinaryTree/BinaryTreeUse.cpp
@@ -133,6 +133,29 @@ int diameter(BinaryTreeNode<int>* root){
     int temp = max(option1 , option2);
     return max(temp , option3);
 }
+pair<int,int> diaHelper(BinaryTreeNode<int>* root){
+    if(root == NULL){
+        pair<int,int> p;
+        p.first =0;
+        p.second = 0;
+        return p;
+    }
+    pair<int, int> leftAns = diaHelper(root->left);
+    pair<int, int> rightAns = diaHelper(root->right);
+    int lh = leftAns.first;
+    int ld = leftAns.second;
+    int rh = rightAns.first;
+    int rd = rightAns.second;
+    int height = 1 + max(lh , rh);
+    int diameter = max(lh + rh , max(ld, rd));
+    pair<int,int> ans;
+    ans.first = height;
+    ans.second = diameter;
+    return ans;
+}
+int dia(BinaryTreeNode<int>* root){
+    return diaHelper(root).second;
+}
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 8 9 -1 -1 -1 -1 -1 -1 
 int main()
 {
