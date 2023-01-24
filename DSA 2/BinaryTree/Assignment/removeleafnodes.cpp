4156dc93cae4cd3860d3dc18daf964d51ea85fe2
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-25 01:09:26 +0530

    Remove Leaf Nodes

---
+++ DSA 2/BinaryTree/Assignment/removeleafnodes.cpp
@@ -0,0 +1,6 @@
+BinaryTreeNode<int>* removeLeafNodes(BinaryTreeNode<int> *root) {
+    if(root==NULL || (root ->left == NULL && root ->right == NULL))return NULL;
+    if(root->left !=NULL)root->left = removeLeafNodes(root->left);
+    if(root->right !=NULL)root->right = removeLeafNodes(root->right);
+    return root;
+}
