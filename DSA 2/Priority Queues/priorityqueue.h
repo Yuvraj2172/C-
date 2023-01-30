f1cf25e07085d61f3fa132206f00d297a58a1b77
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-31 00:02:13 +0530

    Remove Min Function

--- DSA 2/Priority Queues/priorityqueue.h
+++ DSA 2/Priority Queues/priorityqueue.h
@@ -17,5 +17,45 @@ class PriorityQueue{
         }
         return pq[0];
      }
-     
+     void insert(int element ){
+        pq.push_back(element);
+        int childIndex = pq.size()-1;
+        int parentIndex = (childIndex -1) /2;
+        while(childIndex>0 && pq[childIndex] < pq[parentIndex]){
+            if(pq[childIndex] < pq[parentIndex]){
+            swap(pq[parentIndex], pq[childIndex]);
+        }
+        childIndex = parentIndex ;
+        }
+
+        // parentIndex = (childIndex - 1)/2;
+     } 
+     int removeMin() {
+        if(isEmpty())return 0;
+        int removed = pq[0];
+        pq[0] = pq[pq.size()-1];
+        pq.pop_back();
+        // if(isEmpty())return 0;
+        int parentIndex =0;
+        while(parentIndex < pq.size()){
+            int lci =2*parentIndex + 1;
+            int rci = 2* parentIndex +2;
+            int replacedwith;
+            if(lci < pq.size() && rci < pq.size()){
+              replacedwith = (pq[lci] <= pq[rci]) ? lci : rci;
+            }else if(lci < pq.size()){
+                replacedwith = lci;
+            }else{
+                break;
+            }
+            if(pq[replacedwith] < pq[parentIndex]){
+                swap(pq[parentIndex] , pq[replacedwith]);
+            }
+            else {
+                break;
+            }
+            parentIndex = replacedwith;
+        }
+        return removed;
+    }
 };
