8590d671c332de6f2984b90d0908b6f511b315d7
Yuvraj Soni <demonsroar2172@gmail.com>
2023-01-31 20:30:13 +0530

    k Sorted Array

---
+++ DSA 2/Priority Queues/ksortedarray.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void kSortedArray(int input[] ,int n , int k){
+    priority_queue<int> pq;
+    for(int i=0; i<k;i++){
+        pq.push(input[i]);
+    }
+    int j=0;
+    for(int i=k;i<n;i++){
+        input[j] = pq.top();
+        pq.pop();
+        pq.push(input[i]);
+        j++;
+    }
+    while(!pq.empty()){
+        input[j++] = pq.top();
+        pq.pop();
+    }
+}
+int main(){
+    int input[] = {10, 12 ,6 ,7 ,9};
+    int k=3;
+    kSortedArray(input,5, k);
+    for(auto it : input)cout<<it<<" ";
+    cout<<"\n";
+}
