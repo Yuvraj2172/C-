17b330a75441b907081a9600d31b30eff8acba84
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-07 12:38:54 +0530

    Standarad DP

--- DSA3/Dynamic Programming/fibonacci.cpp
+++ DSA3/Dynamic Programming/fibonacci.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+int fibo_3(int n){
+    int *ans = new int[n+1];
+    ans[0] = 0;
+    ans[1] = 1;
+    for(int i =2;i<=n;i++){
+        ans[i] = ans[i-1] + ans[i-2];
+    }
+    return ans[n];
+}
 int fibonacci_helper(int n, int *ans){
     if(n<=1){
         ans[n] = n;
@@ -27,5 +36,5 @@ int fibonacci(int n){
 int main(){
     int n;
     cin>>n;
-    cout<<fibonacci(n);
+    cout<<fibo_3(n);
 }
