b8b5f8b94e672520572f5d7c9794ad1364ca1ede
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-07 15:54:23 +0530

    Min Steps to 1 Using DP

--- DSA3/Dynamic Programming/minstepsto1.cpp
+++ DSA3/Dynamic Programming/minstepsto1.cpp
@@ -50,3 +50,25 @@ int countMinStepsToOne(int n)
 	}
 	return helper(n , ans);
 }
+
+#include<bits/stdc++.h>
+int countStepsToOne(int n)
+{
+	int *arr = new int[n+1];
+	arr[1] =0;
+	arr[2] =1;
+	arr[3]=1;
+	for(int i=4;i<n+1;i++){
+		int x = arr[i-1];
+		int y= INT_MAX;
+		if(i%2==0){
+			y = arr[i/2];
+		}
+		int z = INT_MAX;
+		if(i%3==0){
+			z = arr[i/3];
+		}
+		arr[i] = min(x, min(y,z))+1;
+	}
+	return arr[n];
+}
