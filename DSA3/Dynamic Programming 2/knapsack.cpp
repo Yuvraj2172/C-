81921bb2b86a4da7eeb8cff52fb73a739d963802
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-09 18:24:17 +0530

    KnapSack Memoization

--- DSA3/Dynamic Programming 2/knapsack.cpp
+++ DSA3/Dynamic Programming 2/knapsack.cpp
@@ -11,3 +11,32 @@ int knapsack(int *weights, int *values, int n, int maxWeight)
     int y = knapsack(weights +1 , values+1 , n-1 , maxWeight  );
     return max(x,y);
 }
+
+#include<bits/stdc++.h>
+int helper(int* weight , int * value , int i , int maxWeight, int** dp){
+	if(i<0){
+		return 0;
+	}
+	if(dp[i][maxWeight]!=-1){
+		return dp[i][maxWeight];
+	}
+	if(weight[i]> maxWeight){
+		dp[i][maxWeight] = helper(weight , value , i-1 ,maxWeight, dp);
+		return dp[i][maxWeight];
+	}
+	else {
+          dp[i][maxWeight] =
+              max(value[i] +helper(weight, value, i - 1, maxWeight - weight[i], dp),helper(weight, value, i - 1, maxWeight,dp));
+			return dp[i][maxWeight];
+        }
+}
+int knapsack(int* weight, int* value, int n, int maxWeight) {
+	int** dp = new int*[n+1];
+	for(int i=0;i<=n;i++){
+		dp[i] = new int[maxWeight+1];
+		for(int j=0;j<=maxWeight;j++){
+			dp[i][j] = -1;
+		}
+	}
+	return helper(weight, value , n-1 , maxWeight , dp);	
+}
