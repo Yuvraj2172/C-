d8787e578a8b80840b1f59a9d1d0cad3ca2faa46
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-09 15:27:22 +0530

    KnapSack

---
+++ DSA3/Dynamic Programming 2/knapsack.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+int knapsack(int *weights, int *values, int n, int maxWeight)
+{
+	if(n==0 || maxWeight==0){
+        return 0;
+    }
+    if(weights[0] > maxWeight){
+        return  knapsack(weights +1 , values+1 , n-1 , maxWeight  );
+    }
+    int x = knapsack(weights +1 ,values +1, n-1 , maxWeight - weights[0] )+ values[0];
+    int y = knapsack(weights +1 , values+1 , n-1 , maxWeight  );
+    return max(x,y);
+}
