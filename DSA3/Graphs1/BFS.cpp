adb32c2dc6c897667012ea815c08aa1591425738
Yuvraj Soni <demonsroar2172@gmail.com>
2023-02-13 21:54:52 +0530

    BFS in Graph

--- DSA3/Graphs1/BFS.cpp
+++ DSA3/Graphs1/BFS.cpp
@@ -1,51 +1,51 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
-void printBFS(int** edges , int n , int sv){
-    bool * visited = new bool[n];
-    for(int i=0;i<n;i++){
-        visited[i] = false;
-    }
-    queue<int> pendingVertices;
-    pendingVertices.push(sv);
-    visited[sv] = true;
-    while(!pendingVertices.empty()){
-        int currentVertex = pendingVertices.front();
-         pendingVertices.pop(); 
-        // visited[currentVertex] = true;
-        cout<<currentVertex<<"\n";
-        for(int i=0;i<n;i++){
-            if(i == currentVertex){
-                continue;
-            }
-            if(edges[currentVertex][i]==1 && !visited[i]){
-                pendingVertices.push(i);
-                visited[i] = true;
-            } 
-        }
-    }
-    delete [] visited;
+void print(bool **edges, long long v, long long sv, bool *visited) {
+  queue<long long> q;
+  q.push(sv);
+  visited[sv]= true;
+  while(!q.empty()){
+      cout<<q.front()<<" ";
+      for(int i=0;i<v;i++){
+          if(i== q.front())continue;
+          if(visited[i])continue;
+          if(edges[q.front()][i]){
+              q.push(i);
+              visited[i] = true;
+          }
+      }
+      q.pop();
+  }
 }
-int main(){
-    int n;
-    int e;
-    cin >> n >> e;
-    int **edges = new int *[n];
-    for (int i = 0; i < n; i++)
-    {
-        edges[i] = new int[n];
-        for (int j = 0; j < n; j++)
-        {
-            edges[i][j] = 0;
-        }
-    }
 
-    for (int i = 0; i < e; i++)
-    {
-        int f, s;
-        cin >> f >> s;
-        edges[f][s] = 1;
-        edges[s][f] = 1;
+int main() {
+  // Write your code here
+  long long v, e;
+  cin >> v >> e;
+  bool **edges = new bool *[v];
+  for (long long i = 0; i < v; i++) {
+    edges[i] = new bool[v];
+    for (long long j = 0; j < v; j++) {
+      edges[i][j] = false;
     }
-    
-    printBFS(edges, n , 0);
+  }
+  for (long long i = 0; i < e; i++) {
+    long long start, end;
+    cin >> start >> end;
+    edges[start][end] = true;
+    edges[end][start] = true;
+  }
+  bool *visited = new bool[v];
+  for (long long i = 0; i < v; i++) {
+    visited[i] = false;
+  }
+  for (long long i = 0; i < v; i++) {
+      if(!visited[i])print(edges , v , i , visited);
+  }
+  delete[] visited;
+  for (long long i = 0; i < v; i++)
+    delete edges[i];
+  delete[] edges;
+  return 0;
 }
