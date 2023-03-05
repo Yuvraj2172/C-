396585f1a5c253144e5f41b73b35b118e53632ad
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-05 21:28:25 +0530

    Fibonacci Best Sol

--- DP/fibonacci.cpp
+++ DP/fibonacci.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int f(int n ,vector<int> &dp){
-    if(n<=1)return n;
+    // if(n<=1)return n;
     if(dp[n]!=-1){
         return dp[n];
     }
@@ -11,7 +11,21 @@ int f(int n ,vector<int> &dp){
 int main(){
     int n;
     cin>>n;
-    vector<int> dp(n+1,-1);
-    cout<<f(n,dp);
+    // vector<int> dp(n+1,-1);
+    // dp[0] =0;
+    // dp[1]=1;
+    // cout<<f(n,dp);
+    // for(int i=2;i<=n;i++){
+    //     dp[i] = dp[i-1] + dp[i-2];
+    // }
+    int prev2 =0;
+    int prev1=1;
+    for(int i=2;i<=n;i++){
+        int curr = prev1+ prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    cout<<prev1;
+    // cout<<dp[n];
     return 0;  
 }
