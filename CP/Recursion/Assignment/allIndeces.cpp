f5dcbe793259e123930dd16af231929f015a2a16
Yuvraj Soni <demonsroar2172@gmail.com>
2023-03-13 22:01:02 +0530

    All Indices of a Number

---
+++ CP/Recursion/Assignment/allIndeces.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void findIndexes(int input[],int currIndex, int size, int x, int output[],int &k) {
+    if(currIndex==size+1)
+        return;
+    if(input[currIndex]==x){
+        output[k]=currIndex;
+        k++;
+    }
+    findIndexes(input,currIndex+1,size,x,output,k);
+}
+
+int allIndexes(int input[], int size, int x, int output[]) {
+    int k=0;
+    findIndexes(input,0,size,x,output,k);
+    return k;
+}
