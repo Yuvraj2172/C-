c0ec954e289cb61039b9b53d7aab1ffb76d9a53b
Yuvraj Soni <demonsroar2172@gmail.com>
2023-04-06 11:13:04 +0530

    BFS of a Graph

--- DSA3/Graphs1/intro.cpp
+++ DSA3/Graphs1/intro.cpp
@@ -1,30 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int n, m;
-    cin >> n >> m;
-    // graph here
+//BFS of a Graph
+// Given a directed graph. The task is to do Breadth First Traversal of this graph starting from 0.
+// Note: One can move from node u to node v only if there's an edge from u to v and find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
 
-    //Matrix Method
-    int adj[n + 1][m + 1];
-    for (int i = 0; i < m; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adj[u][v] = 1;
-        adj[v][u] = 1;
+vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int> vis(V,false);
+        int s=0;
+        vis[s] = true;
+        vector<int> res;
+        queue<int> q;
+        q.push(s);
+        while(!q.empty()){
+            int t = q.front();
+            res.push_back(t);
+            q.pop();
+            for(auto it : adj[t]){
+                if(!vis[it]){
+                    vis[it] = true;
+                    q.push(it);
+                }
+            }
+        }
+        return res;
     }
-    
-    
-    //Adjacency Method
-    vector<int> adj[n+1];
-    for(int i=0;i<m;i++){
-        int u,v;
-        cin>>u>>v;
-        adj[u].push_back(u);
-        adj[v].push_back(v);
-    }
-
-    return 0;
-}
