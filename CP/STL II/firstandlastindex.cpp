e6ce767f76d123a6d1fdc1062191a142aacbd359
Yuvraj Soni <demonsroar2172@gmail.com>
2023-04-08 22:27:00 +0530

    First and Last Index of a Number

--- CP/STL II/firstandlastindex.cpp
+++ CP/STL II/firstandlastindex.cpp
@@ -1,26 +1,39 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define emplace_back em;
-#define ll long long;
-void fs(int &number){
-    bool negative = false;
-    register int c;
-    number = 0;
-    c = getchar();
-    if (c=='-'){
-     negative = true;
-    c = getchar();}
-    for (; (c>47 && c<58); c=getchar())
-    number = number *10 + c - 48;
-    if (negative)
-    number *= -1;
-}
-int main(){
-ios_base::sync_with_stdio(0);
-cin.tie(0); cout.tie(0);
-    int tc; fs(tc);
-    while(tc--){
-    
+#include <bits/stdc++.h>
+pair<int, int> findFirstLastPosition(vector<int> &arr, int n, int x)
+{
+    pair<int, int> ans;
+    if (arr.size() == 0)
+        return {-1, -1};
+    int left = lower_bound(arr.begin(), arr.end(), x) - arr.begin();
+    int right = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
+    if (left >= n)
+    {
+        return {-1, -1};
     }
-return 0;
+    if (arr[left] == x)
+    {
+        return {left, right - 1};
+    }
+    return {-1, -1};
+}
+
+using namespace std;
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    int X;
+    cin >> X;
+
+    pair<int, int> sol = findFirstLastPosition(arr, N, X);
+    cout << sol.first << " " << sol.second << endl;
 }
