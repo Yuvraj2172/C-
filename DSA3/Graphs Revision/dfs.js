722e68b089da49b1ad732b974fcff48dff6ae5a2
Yuvraj Soni <demonsroar2172@gmail.com>
2023-05-27 22:42:42 +0530

    breadth first search

--- DSA3/Graphs Revision/dfs.js
+++ DSA3/Graphs Revision/dfs.js
@@ -1,29 +1,29 @@
-const depthFirstSearch = (graph , source)=>{
-    const stack = [source];
-    while(stack.length> 0){
-        const current = stack.pop();
-        console.log(current);
-        for(let neighbor of graph[current]){
-            stack.push(neighbor);
-        }
+const depthFirstSearch = (graph, source) => {
+  const stack = [source];
+  while (stack.length > 0) {
+    const current = stack.pop();
+    console.log(current);
+    for (let neighbor of graph[current]) {
+      stack.push(neighbor);
     }
+  }
 };
 
-const depthFirstSearchRec=(graph , source)=>{
-    console.log(source);
-    for(let neighbor of graph[source]){
-        depthFirstSearchRec(graph , neighbor);
-    }  
+const depthFirstSearchRec = (graph, source) => {
+  console.log(source);
+  for (let neighbor of graph[source]) {
+    depthFirstSearchRec(graph, neighbor);
+  }
 };
 
-const graph= { 
-    a: ['b','c'],
-    b: ['d'],
-    c: ['e'],
-    d: ['f'],
-    e: [],
-    f:[]
-}
+const graph = {
+  a: ["b", "c"],
+  b: ["d"],
+  c: ["e"],
+  d: ["f"],
+  e: [],
+  f: [],
+};
 
-depthFirstSearchRec(graph , 'a');
-depthFirstSearch(graph, 'a');
+depthFirstSearchRec(graph, "a");
+depthFirstSearch(graph, "a");
