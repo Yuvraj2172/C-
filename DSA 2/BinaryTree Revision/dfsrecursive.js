e6a1183ca7e07649692932912ae18774366b9140
Yuvraj Soni <demonsroar2172@gmail.com>
2023-05-29 19:54:30 +0530

    dfs recursive

--- DSA 2/BinaryTree Revision/dfsrecursive.js
+++ DSA 2/BinaryTree Revision/dfsrecursive.js
@@ -11,3 +11,11 @@ const depthFirstValues2=(root , ans)=>{
     return ans;
   }
   
+
+
+  const depthFirstValues = (root) => {
+    if(root === null)return [];
+    const leftValues = depthFirstValues(root.left);
+    const rightValues = depthFirstValues(root.right);
+    return [root.val, ...leftValues, ...rightValues];
+  };
