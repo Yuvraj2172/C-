47065d440b86c35c79d2f3759de63fb0e130fbbb
Yuvraj Soni <demonsroar2172@gmail.com>
2023-05-29 20:19:10 +0530

    tree includes

--- DSA 2/BinaryTree Revision/treeincludes.js
+++ DSA 2/BinaryTree Revision/treeincludes.js
@@ -9,3 +9,9 @@ const treeIncludes = (root, target) => {
     }
     return false;
   };
+
+  const treeIncludes2= (root , target)=>{
+    if(root === null)return false;
+    if(root.val === target)return true;
+    return treeIncludes(root.left, target) || treeIncludes(root.right , target);
+  }
