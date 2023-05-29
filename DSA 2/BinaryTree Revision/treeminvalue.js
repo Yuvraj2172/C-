b4872990e528f23b5b6aa045db244c419893a349
Yuvraj Soni <demonsroar2172@gmail.com>
2023-05-29 20:38:01 +0530

    tree min value

--- DSA 2/BinaryTree Revision/treeminvalue.js
+++ DSA 2/BinaryTree Revision/treeminvalue.js
@@ -1,12 +1,19 @@
 const treeMinValue = (root) => {
-    if(root === null)return Infinity;
-    let ans = Infinity;
-    const q =[root];
-    while(q.length>0){
-      const current = q.shift();
-      if(current.val < ans)ans = current.val;
-      if(current.left)q.push(current.left);
-      if(current.right)q.push(current.right);
-    }
-    return ans;
-  };
+  if (root === null) return Infinity;
+  let ans = Infinity;
+  const q = [root];
+  while (q.length > 0) {
+    const current = q.shift();
+    if (current.val < ans) ans = current.val;
+    if (current.left) q.push(current.left);
+    if (current.right) q.push(current.right);
+  }
+  return ans;
+};
+
+const treeMinValue2 = (root) => {
+  if (root === null) return Infinity;
+  const leftAns = treeMinValue2(root.left);
+  const rightAns = treeMinValue2(root.right);
+  return Math.min(root.val, leftAns, rightAns);
+};
