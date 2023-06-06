a0863dba12ae5c90b273d7d0cc1230b370c64876
Yuvraj Soni <demonsroar2172@gmail.com>
2023-06-07 04:28:57 +0530

    maximum path sum in a matrix memoized

--- DP/DP revision/mpsinmatrix.cpp
+++ DP/DP revision/mpsinmatrix.cpp
@@ -21,3 +21,26 @@ int getMaxPathSum(vector<vector<int>> &matrix)
     }
     return maxi;
 }
+
+
+#include <bits/stdc++.h> 
+int f(int i, int  j , vector<vector<int>> &matrix, vector<vector<int>>&dp){
+    if(j< 0 || j >=matrix[0].size())return -1e8;
+    if(dp[i][j]!=-1)return dp[i][j];
+    if(i==0)return matrix[i][j];
+    int u = matrix[i][j] + f(i-1, j ,matrix,dp);
+    int ld = matrix[i][j] + f(i-1, j-1,matrix,dp);
+    int rd = matrix[i][j] + f(i-1, j+1, matrix,dp);
+    return dp[i][j]=max(u, max(ld, rd));
+}
+int getMaxPathSum(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+   int maxi = -1e8;
+   for(int j = 0 ; j <m; j++){
+       maxi = max(maxi , f(n-1, j, matrix, dp));
+   }
+   return maxi;
+}
